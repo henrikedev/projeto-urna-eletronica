acf9b7b5d62cbd8c0d247e4d7658a212f053cc05
henrikedev <h.mirandacm@gmail.com>
2021-07-28 19:22:42 -0300

    Criado o Json contendo as informações dos candidatos, as variáveis de controle dos itens existentes na tela da Urna e as funções que irão controlar os botões

---
+++ etapas.js
@@ -0,0 +1,88 @@
+let etapas = 
+[
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 5,
+        candidatos:
+        [
+            {
+                numero: 23444,
+                nome: 'Cleiton Azevedo',
+                partido: 'PPS',
+                fotos: 
+                [
+                    {
+                        url: '23444.jpg', legenda: 'DEPUTADO ESTADUAL'
+                    }
+                ]
+            }
+
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos:
+        [
+            {
+                numero: 17,
+                nome: 'Jair Bolsonaro',
+                partido: 'PSL',
+                fotos: 
+                [
+                    {
+                        url: '17.jpg', legenda: 'PRESIDENTE'
+                    },
+                    {
+                        url: '17_2.jpg', legenda: 'VICE-PRESIDENTE', small: true
+                    },
+                ]
+            }
+
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos:
+        [
+            {
+                numero: 13,
+                nome: 'Luís Inácio',
+                partido: 'PT',
+                fotos: 
+                [
+                    {
+                        url: '13.jpg', legenda: 'PRESIDENTE'
+                    },
+                    {
+                        url: '13_2.jpg', legenda: 'VICE-PRESIDENTE', small: true
+                    },
+                ]
+            }
+
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos:
+        [
+            {
+                numero: 12,
+                nome: 'Ciro Gomes',
+                partido: 'PDT',
+                fotos: 
+                [
+                    {
+                        url: '12.jpg', legenda: 'PRESIDENTE'
+                    },
+                    {
+                        url: '12_2.jpg', legenda: 'VICE-PRESIDENTE', small: true
+                    },
+                ]
+            }
+
+        ]
+    }
+]
